a97e915e6cbb7de092c313159b339b471e85d845
merles <noe.merles@gmail.com>
2017-11-08 00:02:39 +0100

    Inicialització
    primer commit

---
+++ Kaggle/Titanic/main.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  7 23:55:08 2017
+
+@author: Noè
+"""
+
+import pandas as pd
+import numpy as np
+import random as rd 
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# machine learning
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+
