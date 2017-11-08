fed6aa8bbe378a3c08bd20cfeeb256e29a52046e
merles <noe.merles@gmail.com>
2017-11-08 12:31:38 +0100

    comencem l'analisi

--- Kaggle/Titanic/main.py
+++ Kaggle/Titanic/main.py
@@ -34,3 +34,40 @@ print(train_df.columns.values)
 train_df.info()
 print('_'*40)
 test_df.info()
+
+train_df.describe()
+train_df.describe(include=['O'])
+train_df.describe(include='all')
+
+#Completar
+#
+# podem mirar les dades amb les ultimes comandes i veiem que hi ha
+# categories que hem de completar com 'age' perquè segurament és important
+# per predir la probabilitat de sobreviure. Potser també ho haurem de fer amb 
+# la variable embarked.
+#
+# Corregir
+#
+# la feature ticket no ens diu massa re tenim moltes repetides, la eleiminarem
+# Cabin també li falten moltes dades i no seria massa útil
+# passanger Id no ens diu res i també l'extraurem
+# Name no és massa standard i de moment la deixem tot i que potser no és massa
+#rellevant
+#
+#Crear
+#
+#podem intentar fer una categoria amb el nombre de familiars a partir de sibsp
+# i parch 
+#podem buscar la manera de treure info del nom de la gent per veure si és útil
+#per ajudar-nos a fer la classificació podriem convertir la categoria edat de 
+# numerica continua a franges
+#potser crear la categoria fare ens ajuda
+
+
+#comprovem si el fet de pertanyer a diferenta categoria afecta
+print('comprovem si el fet de pertanyer a diferenta categoria afecta')
+print(train_df[['Pclass','Survived']].groupby(by='Pclass').mean())
+
+#comprovem si el sexe afecta
+print('comprovem si el sexe afecta')
+print(train_df[['Sex','Survived']].groupby(by='Sex').mean())
